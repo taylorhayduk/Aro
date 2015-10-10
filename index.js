1c4ee61539e6b0f4a6d2de090680a0aca507ade4
Taylor Hayduk <taylorhayduk@gmail.com>
2015-10-09 23:08:24 -0400

    SwappingGame 70% complete

--- index.js
+++ index.js
@@ -11,7 +11,22 @@ var port = process.env.PORT || 3000;
 //         console.log('connection successful');
 //     }
 // });
+var games = {};
 
+var SwappingGame = function (players) {
+  var gameID = players[0].gameID;
+  //subscribe to new socket (should be on client side)
+  io.on('connection', function(socket){
+    socket.join(gameID);
+  });
+
+  io.to(gameID).emit([players[0].playerId, players[1].location]);
+  io.to(gameID).emit([players[1].playerId, players[0].location]);
+
+  //listen for target acquired to end game
+  //io.on('targetAcquired')  
+    //end game
+}
 
 io.on('connection', function(socket){
   console.log('a user connected');
@@ -24,6 +39,17 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', 'this is from the server. Joyce, Rod, Tisha, and Taylor are awesome!!');
   });
+
+  socket.on('gameEnter', function(playerObj) {
+    games[playerObj.gameID] = games[playerObj.gameID] || [];
+    games[playerObj.gameID].push(playerObj);
+    if (games[gameID].length >= 2) {
+      io.emit('game start', gameID);
+      //create new Game
+      // var gameID = new SwappingGame(games[gameID])
+
+    }
+  }
 });
 
 http.listen(port, function(){
