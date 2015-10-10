255f0c01e41ed2bd54e30f00b12e8dd9c3eb04c5
Taylor Hayduk <taylorhayduk@gmail.com>
2015-10-09 20:21:32 -0400

    Added Procfile, index.js, package.json

---
+++ index.js
@@ -0,0 +1,31 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+// var mongoose = require('mongoose');
+  
+// mongoose.connect('mongodb://localhost/SpeechChat', function(err) {
+//     if(err) {
+//         console.log('connection error', err);
+//     } else {
+//         console.log('connection successful');
+//     }
+// });
+
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:'+port);
+});
